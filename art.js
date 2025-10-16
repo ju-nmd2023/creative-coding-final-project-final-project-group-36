8f26a5c1b1184a97f8c021fe91b176b83691f7bd
Acefades <144893637+Acefades@users.noreply.github.com>
2025-10-16 19:31:35 +0200

    Geometric grid

--- art.js
+++ art.js
@@ -19,4 +19,21 @@ function draw() {
   }
 
   t += 0.02;
+
+  for (let y = 0; y < height; y += 100) {
+    for (let x = 0; x < width; x += 100) {
+      push();
+      translate(x + 50, y + 50);
+      rotate(sin(t + (x + y) * 0.002) * 0.5);
+
+      let glow = color(0, random(70, 100), random(60, 100), 80);
+      fill(glow);
+      stroke(0, 0, 100, 20);
+      strokeWeight(2);
+
+      rect(0, 0, 60 + sin(t + x * 0.01) * 20, 60 + cos(t + y * 0.01) * 20);
+      pop();
+    }
+  }
 }
+
