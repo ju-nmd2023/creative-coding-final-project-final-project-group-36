81a222244c7aa9d4ffffcc78379bcb0b5f02c8b3
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2025-10-16 19:26:32 +0200

    setup

--- art.js
+++ art.js
@@ -1,1 +1,12 @@
-// HEJ
+let t = 0;
+
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB, 360, 100, 100, 100);
+  frameRate(30);
+  rectMode(CENTER);
+}
+
+function draw() {
+  t += 0.02;
+}
