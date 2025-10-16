8116a36c4f9dd80fa2fc8aed5a227ea47bb9576e
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2025-10-16 19:27:07 +0200

    Gradient background

--- art.js
+++ art.js
@@ -10,3 +10,13 @@ function setup() {
 function draw() {
   t += 0.02;
 }
+ function draw() {
+  for (let i = 0; i < height; i++) {
+    let inter = map(i, 0, height, 0, 1);
+    let c = lerpColor(color(250, 40, 10), color(300, 70, 40), inter);
+    stroke(c);
+    line(0, i, width, i);
+  }
+
+  t += 0.02;
+}
